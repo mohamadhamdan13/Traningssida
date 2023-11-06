b015c422b0a6bf0f76d58c6935c6cc198358837f
mohamadhamdan13 <142982637+mohamadhamdan13@users.noreply.github.com>
2023-11-06 14:48:24 +0100

    lägga till övningar

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,4 @@
+
 function getCurrentWeekNumber() {
   var today = new Date();
   var startOfWeek = new Date(today.getFullYear(), 0, 1);
@@ -14,3 +15,32 @@ document.addEventListener("DOMContentLoaded", function () {
       weekTitle.textContent = "Vecka " + currentWeek;
   }
 });
+
+
+$(function () {
+  // Hantera klick på "Lägg till övning" för varje dag separat
+  $(".add-exercise").on("click", function () {
+      const $form = $(this).prev(".exercise-form");
+      const exerciseCount = $form.find("input[type='text']").length + 1;
+
+      const newExercise = '<label for="exercise' + exerciseCount + '">Övning ' + exerciseCount + ':</label>' +
+          '<input type="text" name="exercise' + exerciseCount + '">' +
+          '<label for="sets' + exerciseCount + '">Set:</label>' +
+          '<input type="number" name="sets' + exerciseCount + '">' +
+          '<label for="reps' + exerciseCount + '">Reps:</label>' +
+          '<input type="number" name="reps' + exerciseCount + '">' +
+          '<label for="rest' + exerciseCount + '">Vila (sekunder):</label>' +
+          '<input type="number" name="rest' + exerciseCount + '">';
+
+      $form.append(newExercise);
+  });
+});
+
+
+
+
+
+
+
+
+
